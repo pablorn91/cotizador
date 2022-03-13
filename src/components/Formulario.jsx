113af1ee095b1371e769a7e04ef3aff6c8c2251c
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-13 10:15:14 -0400

    Configurando context y agregando Error Component

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,32 @@
 import { Fragment } from "react";
-
+import { useState } from "react/cjs/react.development";
 import { MARCAS, YEARS, PLANES } from "../constants";
+import Error from "./Error";
+import useCotizador from "../hooks/useCotizador";
+
 const Formulario = () => {
+
+    const { datos, handleChangeDatos, error, setError } = useCotizador()
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(Object.values(datos).includes('')) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+
+        setError('')
+        //TODO: Cotizar 
+    }
+
   return (
     <>
-      <form>
+
+        {error && <Error />}
+      <form
+        onSubmit={handleSubmit}
+      >
         <div className="my-5 ">
           <label className="block mb-3 font-bold text-gray-400 uppercase">
             Marca
@@ -13,6 +35,8 @@ const Formulario = () => {
           <select
             name="marca"
             className="w-full p-3 bg-white border border-gray-200 rounded-md"
+            onChange={e => handleChangeDatos(e)}
+            value={datos.marca}
           >
             <option value="">-- Selecciona Marca --</option>
             {MARCAS.map((marca) => (
@@ -29,8 +53,10 @@ const Formulario = () => {
           </label>
 
           <select
-            name="marca"
+            name="year"
             className="w-full p-3 bg-white border border-gray-200 rounded-md"
+            onChange={e => handleChangeDatos(e)}
+            value={datos.year}
           >
             <option value="">-- Selecciona Año --</option>
             {YEARS.map( year => (
@@ -56,6 +82,7 @@ const Formulario = () => {
                       type="radio"
                       name="plan"
                       value={plan.id}
+                      onChange={e => handleChangeDatos(e)}
                       />
                   </Fragment>
               ))}
--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,13 +1,31 @@
-import { createContext } from 'react'
+import { useState, createContext } from 'react'
 
 const CotizadorContext =createContext()
 
 const CotizadorProvider = ({children}) => {
 
+    const [ datos, setDatos ] = useState({
+        marca: '',
+        year: '',
+        plan: ''
+    })
+
+    const [ error, setError ] = useState('')
+
+    const handleChangeDatos = e => {
+        setDatos({
+            ...datos,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <CotizadorContext.Provider
             value={{
-                
+                datos,
+                handleChangeDatos,
+                error,
+                setError
             }}
         >
             {children}
