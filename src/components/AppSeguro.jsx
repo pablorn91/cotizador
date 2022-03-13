1ee314b9a2eab39e4b1bc594e0e8dfee1cd63807
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-13 12:05:07 -0400

    useCallBack, useMemo, useRef

--- src/components/AppSeguro.jsx
+++ src/components/AppSeguro.jsx
@@ -1,17 +1,25 @@
-import Formulario from "./Formulario"
+import Formulario from "./Formulario";
+import Spinner from "./Spinner";
+import Resultado from "./Resultado";
+import useCotizador from "../hooks/useCotizador";
 
 const AppSeguro = () => {
+  const { resultado, cargando } = useCotizador();
+
   return (
     <>
-        <header className="my-10">
-            <h1 className="text-white text-center text-4xl font-black">Cotizador de Seguros de Auto</h1>
-        </header>
+      <header className="my-10 mt-10">
+        <h1 className="text-white text-center text-4xl font-black">
+          Cotizador de Seguros de Auto
+        </h1>
+      </header>
 
-        <main className="bg-white md:w-2/3 lg:w-2/4 mx-auto shadow rounded-lg p-10">
-            <Formulario />
-        </main>
+      <main className="bg-white w-5/6 md:w-2/3 lg:w-2/4 mx-auto shadow rounded-lg p-10 mb-10">
+        <Formulario />
+        {cargando ? <Spinner /> : <Resultado />}
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default AppSeguro
+export default AppSeguro;
--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -12,7 +12,8 @@ const CotizadorProvider = ({children}) => {
     })
 
     const [ error, setError ] = useState('')
-    const [ resultado, setResultado ] = useState('')
+    const [ resultado, setResultado ] = useState(0)
+    const [ cargando, setCargando ] = useState(false)
 
     const handleChangeDatos = e => {
         setDatos({
@@ -42,8 +43,12 @@ const CotizadorProvider = ({children}) => {
         
         //Formatear Dinero
         resultado = formatearDinero(resultado)
-
-        setResultado(resultado)
+        
+        setCargando(true)
+        setTimeout(() => {
+            setResultado(resultado)
+            setCargando(false)
+        }, 1000);
 
     }
 
@@ -54,7 +59,9 @@ const CotizadorProvider = ({children}) => {
                 handleChangeDatos,
                 error,
                 setError,
-                cotizarSeguro
+                cotizarSeguro,
+                resultado,
+                cargando
             }}
         >
             {children}
